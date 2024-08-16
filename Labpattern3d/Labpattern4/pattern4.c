d4155a291985a7b7cbb1101fe3c61e872e485b84
Jatintiwari3341 <tripathijatin319@gmail.com>
2024-08-16 12:24:29 +0530

    new

--- Labpattern3d/Labpattern4/pattern4.c
+++ Labpattern3d/Labpattern4/pattern4.c
@@ -14,3 +14,26 @@
    16
 */            
 
+#include<stdio.h>
+int main(){
+    int n,r,c;
+    printf("Enter the number");
+    scanf("%d",&n);
+     int a=1;
+    for(r=1;r<=n;r++){
+       
+        for(c=1;c<=r;c++){
+            printf("%d",a++);
+        }
+        printf("\n");
+    }
+      for(r=1;r<=n;r++){
+       
+        for(c=n-1;c>=r;c--){
+            printf("%d",a++);
+        }
+        printf("\n");
+    }
+    
+    
+}
